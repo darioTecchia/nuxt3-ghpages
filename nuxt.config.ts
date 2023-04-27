5a801090ce789ed50f3c5afb99ad513753e4d9c7
Dario Tecchia <dario.tecchia@emeal.nttdata.com>
2023-04-27 18:48:47 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: true,
+  app: {
+    head: {
+      title: 'Nuxt3 x GitHub Pages',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Dario Tecchia\' personal website!' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg', href: '/favicon.svg' }
+      ]
+    },
+    baseURL: '/nuxt3-ghpages/'
+  },
+  modules: [
+  ],
+  css: [
+    "bootstrap/dist/css/bootstrap.min.css",
+    "bootstrap-vue-3/dist/bootstrap-vue-3.css",
+    '@/assets/style/main.scss',
+  ]
+})
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Ciao</h1>
+</template>
